bb1e5bf0cfa477e99f1a573039b0cfb806fd6e82
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-10-14 11:53:25 +0900

    Add calc procedure

--- src/calc.c
+++ src/calc.c
@@ -1,11 +1,43 @@
-#include <stdio.h>
+#include "ast.h"
 #include "lex.h"
 #include "parser.h"
-#include "ast.h"
+#include "interp.h"
+#include <stdio.h>
+#include <string.h>
 
-int
-main(int argc, char* argv[]) {
-    printf("not yet implement\n");
-    
+void help() {
+    char **desc = {NULL};
+
+    for (int i = 0; strcmp(desc[i], "") != 0; i++) {
+        printf("%s\n", desc[i]);
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    lex_state *lexer     = NULL;
+    parser_state *parser = NULL;
+    ast_t *ast = NULL;
+
+    if (argc < 2) {
+        help();
+        return 0;
+    }
+    lexer = lex(argv[1]);
+    if (lexer == NULL) {
+        printf("lexer fail\n");
+        return -1;
+    }
+    parser = parse(lexer);
+    if (parser == NULL) {
+        return -1;
+    }
+    if(parser->err = 1)
+    {
+        return -1;
+    }
+    ast = parser_ast(parser);
+    run(ast);
+
     return 0;
 }
