05caca51d6d0831a97b69a5e7c40872f874b49d9
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-10-15 01:37:14 +0900

    parser_open return state

--- src/parser.c
+++ src/parser.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "ast.h"
 #include "lex.h"
 #include "parser.h"
-#include "ast.h"
 
-parser_state* parser_state_open();
+static parser_state *parser_state_open();
 
 #define PARSER_END 0
 #define PARSER_CONTINUE 1
 
-static int parse_init(parser_state *state); 
+static int parse_init(parser_state *state);
 
-static int parse_init(parser_state *state)
-{
-    return PARSER_END;
+static int parse_init(parser_state *state) { return PARSER_END; }
+
+static parser_state *parser_state_open() {
+    parser_state *state = malloc(sizeof(parser_state));
+    state->fn           = parse_init;
+    return state;
 }
 
-parser_state* parser_state_open() 
-{
-        parser_state* state= malloc( sizeof(parser_state) );
-        state->fn = parse_init;
-        return NULL;
-}
-
-parser_state* parse(lex_state *lexer)
-{
+parser_state *parse(lex_state *lexer) {
     parser_state *state = parser_state_open();
-    state->stream = lex_state_stream(lexer);
-    int ret = PARSER_CONTINUE;
+    state->stream       = lexer->head;
+    int ret             = PARSER_CONTINUE;
 
-    while(ret == PARSER_CONTINUE)
-    {
+    while (ret == PARSER_CONTINUE) {
         ret = state->fn(state);
         break;
     }
